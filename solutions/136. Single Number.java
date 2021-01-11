134a9109b6662d79492b64f4087f4e541835e449
Sanchit Sharma <sanchit522@gmail.com>
2021-01-11 17:43:42 +0530

    2 ms

---
+++ solutions/136. Single Number.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+        
+        for(int i : nums){
+            res = res^i;
+        }
+        
+        return res;
+    }
+}
