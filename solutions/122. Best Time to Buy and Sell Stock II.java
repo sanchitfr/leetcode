b6180c4ab954e18c76bc71f6b74e0b2fe19f305e
Sanchit Sharma <sanchit522@gmail.com>
2021-01-12 13:50:22 +0530

    0 ms

--- solutions/122. Best Time to Buy and Sell Stock II.java
+++ solutions/122. Best Time to Buy and Sell Stock II.java
@@ -1,24 +1,33 @@
 class Solution {
     
-    private int maxProfit(int[] prices, int prev, int curr){
-        if(curr == prices.length){
-            return 0;
-        }
+//     private int maxProfit(int[] prices, int prev, int curr){
+//         if(curr == prices.length){
+//             return 0;
+//         }
         
-        int op1 = 0, op2 = 0;
+//         int op1 = 0, op2 = 0;
         
-        if(prices[prev] < prices[curr]){
-            op1 = prices[curr] - prices[prev] + maxProfit(prices, prev+1, curr+1);
-        }
-        else{
-            op2 = maxProfit(prices, prev+1, curr+1);    
-        }
+//         if(prices[prev] < prices[curr]){
+//             op1 = prices[curr] - prices[prev] + maxProfit(prices, prev+1, curr+1);
+//         }
+//         else{
+//             op2 = maxProfit(prices, prev+1, curr+1);    
+//         }
         
-        return Math.max(op1, op2);
+//         return Math.max(op1, op2);
         
-    }
+//     }
     
     public int maxProfit(int[] prices) {        
-       return maxProfit(prices, 0, 1);
+       // return maxProfit(prices, 0, 1);
+        
+        int profit = 0;
+        
+        for(int i = 0; i<prices.length-1; i++){
+            if(prices[i+1] > prices[i]){
+                profit += prices[i+1] - prices[i];
+            }
+        }
+        return profit;
     }
 }
