a893dea73693f23fa969312b68b8f6ee09fa01b9
Sanchit Sharma <sanchit522@gmail.com>
2021-01-14 13:46:58 +0530

    0 ms

--- solutions/234. Palindrome Linked List.java
+++ solutions/234. Palindrome Linked List.java
@@ -11,12 +11,14 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         int length = 0;
-        
         ListNode temp = head;
+        
+        //calculate length
         while(temp!=null){
             temp = temp.next;
             length++;
         }
+        //initialise array to store first half of list
         int[] arr = new int[length/2];
         
         int i = 0;
@@ -27,13 +29,14 @@ class Solution {
             i++;
         }
         
+        //if odd number of elements, no need to compare the middle element
         if(length%2 != 0) temp=temp.next;
+        
         i--;
+        //compare the second half of list element by element
         while(temp!= null){
             if(arr[i] != temp.val) return false;
             temp = temp.next;
             i--;
         }
         return true;
-    }
-}
