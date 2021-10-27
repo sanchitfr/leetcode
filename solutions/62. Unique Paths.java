aedcaaa1c9f3346e264fe0cc3418a8daa39002cf
Sanchit Sharma <sanchit522@gmail.com>
2021-10-27 14:10:42 +0530

    0 ms

--- solutions/62. Unique Paths.java
+++ solutions/62. Unique Paths.java
@@ -1,22 +1,28 @@
 class Solution {
+    
+    private int uniquePaths(int m, int n, int i, int j, int[][] storage){
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(i >= m || j >= n) return 0;
+        
+        if(storage[i][j] != -1) return storage[i][j];
+        
+        int op1 = uniquePaths(m,n,i+1, j, storage);
+        int op2 = uniquePaths(m,n,i, j+1, storage);
+        
+        return storage[i][j] = op1+op2;
+    }
+    
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        dp[m-1][n-1] = 0;
+        int [][] storage = new int[m][n];
         
         for(int i = 0; i<m; i++){
-            dp[i][n-1] = 1;
-        }
-        
-        for(int i = 0; i<n; i++){
-            dp[m-1][i] = 1;
-        }
-        
-        
-        for(int i = n-2; i>=0; i--){
-            for(int j = m-2; j>=0; j--){
-                dp[j][i] = dp[j+1][i] + dp[j][i+1];
+            for(int j = 0; j<n; j++){
+                storage[i][j] = -1;
             }
         }
-        return dp[0][0];
+        
+        return uniquePaths(m,n,0,0, storage);
     }
 }
