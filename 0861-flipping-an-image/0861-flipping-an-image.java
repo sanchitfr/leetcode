db166faa4287c39a8d3314abb240a3152f717d05
Sanchit Sharma <sanchit522@gmail.com>
2023-08-21 15:13:45 +0530

    Time: 20 ms (42.38%), Space: 43.9 MB (26.35%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n/2; j++){
+                if(j != n-1-j){
+                    System.out.println(image[i][j] + " " + image[i][n-1-j]);
+                    int temp = image[i][j];
+                    image[i][j] = image[i][n-1 - j];
+                    image[i][n-1-j] = temp;
+                }
+            }
+            System.out.println(Arrays.toString(image[i]));
+        }
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                image[i][j] = (image[i][j] + 1)%2;
+            }
+        }
+        return image;
+    }
+}
