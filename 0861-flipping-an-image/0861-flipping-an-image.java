6a4c1366034941a0a73c9610c8fb5640454a72e1
Sanchit Sharma <sanchit522@gmail.com>
2023-08-21 15:41:29 +0530

    Time: 1 ms (42.38%), Space: 44.2 MB (6.65%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = 0; i<n; i++){
             for(int j = 0; j<n/2; j++){
                 if(j != n-1-j){
-                    System.out.println(image[i][j] + " " + image[i][n-1-j]);
                     int temp = image[i][j];
                     image[i][j] = image[i][n-1 - j];
                     image[i][n-1-j] = temp;
@@ -14,7 +13,6 @@ class Solution {
             }
 
             for(int j=0; j<n; j++) image[i][j] = (image[i][j] +1)%2;
-            System.out.println(Arrays.toString(image[i]));
         }
         return image;
     }
