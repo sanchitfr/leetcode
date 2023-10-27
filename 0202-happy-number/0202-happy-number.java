9b4482c793ff7bdbe704a2bf5923954e8c710c8f
Sanchit Sharma <sanchit522@gmail.com>
2023-10-27 18:26:12 +0530

    Time: 0 ms (100%), Space: 36 MB (100%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+class Solution {
+    
+    // private static boolean isHappy(int n, HashMap<Integer, Boolean> map){
+    //     if(n == 0) return false;
+    //     int sum = 0;
+    //     while(n != 0){
+    //         sum += Math.pow(n%10, 2);
+    //         n = n/10;
+    //     }
+    //     if(sum == 1) return true;
+    //     if(map.containsKey(sum)) return false;
+    //     else{
+    //         map.put(sum, true);
+    //         return isHappy(sum, map);  
+    //     } 
+    // }
+    
+    public boolean isHappy(int n) {
+//         HashMap<Integer, Boolean> map = new HashMap<>();
+        
+//         return isHappy(n, map);
+        
+        for(int i = 0; i <=13; i++){
+            int sum = 0;
+            
+            while(n != 0){
+                sum += Math.pow(n%10, 2);
+                n = n/10;
+            }
+            n = sum;
+        }
+        
+        if(n == 1) return true;
+        
+        return false;
+    }
+}
